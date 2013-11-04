db0177e2cc8a99180efd9381d8b7fb527ff48ee8
zizon <zzdtsv@gmail.com>
2013-11-04 17:31:20 +0800

    add angular manual compile

--- options.js
+++ options.js
@@ -1,1 +1,24 @@
-angular.module("navigation")
+'use strict';
+(function(){
+    var script = document.createElement("script");
+    script.async = 'async';
+    script.src = "angular.js";
+    script.type = "text/javascript";
+    document.querySelector("script").insertBefore(script);
+    
+    script.addEventListener("load",function(event){
+        var injector = angular.injector(['ng']);
+        window.injector = injector;
+        var scope = injector.get("$rootScope").$new(true);
+        scope.navis =[1,2,3];
+        injector.get("$compile")(document.querySelector("ul"))(scope);
+        scope.$apply();
+        
+        var id = window.setTimeout(function(){
+            scope.navis[0] = 9;
+            scope.$apply();
+            //scope.$digest();
+            window.clearTimeout(id);
+        },2000);
+    });
+})();
