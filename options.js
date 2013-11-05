d21228974b6efdc91c8cf558357c91f580520b98
zizon <zzdtsv@gmail.com>
2013-11-05 14:29:40 +0800

    add proxy list

--- options.js
+++ options.js
@@ -9,8 +9,9 @@
     script.addEventListener("load",function(event){
         var injector = angular.injector(['ng']);
         window.injector = injector;
+        
         var scope = injector.get("$rootScope").$new(true);
-        scope.navis =[
+        var navis = scope.navis =[
             {
                 name:"servers"
             },
@@ -21,7 +22,25 @@
                 name:"white-list"
             }
         ];
-        injector.get("$compile")(document.querySelector("div.navi-item"))(scope);
+        
+        injector.get("$compile")(document.querySelector("#navigation"))(scope);
         scope.$apply();
+        
+        chrome.runtime.getBackgroundPage(function(app){
+            var switchyd = app.switchyd;
+            var scope = injector.get("$rootScope").$new(true);
+            scope.servers = switchyd.config.servers;
+            scope.append = function(item){
+                if( "$$hashKey" in item){
+                    item = angular.copy(item);
+                    delete item["$$hashKey"];
+                }
+                
+                scope.servers.push(item);
+            };
+            
+            injector.get("$compile")(document.querySelector("#servers-stage"))(scope);
+            scope.$apply();
+        });
     });
 })();
