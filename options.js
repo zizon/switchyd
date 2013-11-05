540ab9232f8dc989ca38a558c88e3d547025947c
zizon <zzdtsv@gmail.com>
2013-11-05 18:04:31 +0800

    done servers config options

--- options.js
+++ options.js
@@ -7,39 +7,56 @@
     document.querySelector("script").insertBefore(script);
     
     script.addEventListener("load",function(event){
-        var injector = angular.injector(['ng']);
-        window.injector = injector;
-        
-        var scope = injector.get("$rootScope").$new(true);
-        var navis = scope.navis =[
-            {
-                name:"servers"
-            },
-            {
-                name:"proxy-list"
-            },
-            {
-                name:"white-list"
-            }
-        ];
-        
-        injector.get("$compile")(document.querySelector("#navigation"))(scope);
-        scope.$apply();
-        
         chrome.runtime.getBackgroundPage(function(app){
             var switchyd = app.switchyd;
-            var scope = injector.get("$rootScope").$new(true);
-            scope.servers = switchyd.config.servers;
-            scope.append = function(item){
-                if( "$$hashKey" in item){
-                    item = angular.copy(item);
-                    delete item["$$hashKey"];
-                }
-                
-                scope.servers.push(item);
-            };
+            var injector = angular.injector(['ng']);
             
-            injector.get("$compile")(document.querySelector("#servers-stage"))(scope);
+            var scope = injector.get("$rootScope").$new(true);
+            scope.activate = function(index){
+                scope.navis.forEach(function(navi,i){
+                    navi.active = i == index ? true: false;
+                });
+                
+                scope.$emit("active-changed");
+            };
+            scope.navis =["servers","proxy-list","white-list"].map(function(name){
+                return {
+                    name:name,
+                    active:false,
+                    loaded:false
+                }
+            });
+            
+            scope.$on("active-changed",function(event){
+                scope.navis.forEach(function(navi){
+                     if(!navi.loaded){
+                        var scope = navi.scope = injector.get("$rootScope").$new(true);
+                        scope.switchyd = switchyd;
+                        scope.active = navi.active;
+                        scope.append = function(item){
+                            if( "$$hashKey" in item){
+                                item = angular.copy(item);
+                                delete item["$$hashKey"];
+                            }
+                            scope.switchyd.config.servers.push(item);
+                        };
+                        
+                        navi.loaded = true;
+                        injector.get("$compile")(document.querySelector("#"+navi.name))(scope);
+                    }
+                    
+                     navi.scope.active = navi.active;
+                });
+            });
+            
+            scope.navis[0].active = true;
+            scope.$emit("active-changed");
+            
+            ["#navigation"].reduce(function(compile,node){
+                compile(document.querySelector(node))(scope);
+                return compile;
+            },injector.get("$compile"));
+            
             scope.$apply();
         });
     });
