139020585f6b9c833b23f1c3f4587fe5ba72b366
zizon <zzdtsv@gmail.com>
2014-08-30 23:02:07 +0800

    add grunt vulcanize

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,20 @@
-module.exports = function(grunt) {
+'use strict';
 
+module.exports = function(grunt) {
+    grunt.initConfig({
+        vulcanize: {
+            default : {
+                options : {
+                    csp : true
+                },
+
+                files : {
+                    "option.html":"polymer_option.html"
+                }
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-vulcanize');
+    grunt.registerTask('default', ['vulcanize']); 
 }
